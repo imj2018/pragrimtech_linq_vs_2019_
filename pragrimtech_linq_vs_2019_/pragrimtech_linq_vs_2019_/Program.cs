e3ccdd5a28c623c19e4bb77bcd71ef171472e276
imj <imj.programming.2013@gmail.com>
2021-09-02 16:48:04 +1000

    Part 1   What is LINQ (just in memory objects)

--- pragrimtech_linq_vs_2019_/pragrimtech_linq_vs_2019_/Program.cs
+++ pragrimtech_linq_vs_2019_/pragrimtech_linq_vs_2019_/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace pragrimtech_linq_vs_2019_
 {
@@ -6,7 +8,17 @@ namespace pragrimtech_linq_vs_2019_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = { 1, 2, 4, 5, 6, 7, 8, 9, 10 };
+
+            IEnumerable<int> evenNumbers = from number in numbers
+                                            where (number % 2) == 0
+                                            select number;
+
+            foreach (int item in evenNumbers)
+            {
+                Console.WriteLine(item);
+            }
+            
         }
     }
 }
