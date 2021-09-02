be37df27ed4cbbea91fa3c6f0bc6be2fbfab80c8
imj <imj.programming.2013@gmail.com>
2021-09-02 17:19:25 +1000

    Part 2   Writing LINQ Queries

---
+++ WebApplication1/WebApplication1/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //IEnumerable<Student> queryResult = Student.GetAllStudents().Where(student => student.Gender == "Male");
+
+            IEnumerable<Student> queryResult = from student in Student.GetAllStudents()
+                                               where student.Gender == "Male"
+                                               select student;
+
+
+            GridView1.DataSource = queryResult;
+            GridView1.DataBind();
+
+        }
+    }
+}
