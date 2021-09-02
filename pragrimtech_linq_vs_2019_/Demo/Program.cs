9c7e99af8ff2e56326033e2ddc4157efbdcfb82d
imj <imj.programming.2013@gmail.com>
2021-09-02 22:55:03 +1000

    Part 4   LINQ Aggregate Functions (example 2)

--- pragrimtech_linq_vs_2019_/Demo/Program.cs
+++ pragrimtech_linq_vs_2019_/Demo/Program.cs
@@ -6,61 +6,30 @@ namespace Demo
     {
         static void Main(string[] args)
         {
-            int[] Numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            string[] countries = { "india", "USA", "UK" };
 
-            //int? result = null;
-            double? result = 0;
-            double? sum = 0;
-            int total = 0;
+            int? result = null;
 
-
-            foreach (var number in Numbers)
+            foreach (string str in countries)
             {
-                //if (number % 2 == 0)
+                //  if that has less characters store in result
+                // 
+                //if (!result.HasValue || str.Length < result)
                 //{
-                //    //  if it doesn't have a value or less than the result
-                //    //  store in result
-                //    // 
-                //    if (!result.HasValue || number < result)
-                //    {
-                //        result = number;
-                //    }
+                //    result = str.Length;
                 //}
 
-
-                //if (number % 2 == 0)
-                //{
-                //    if (!result.HasValue || number > result)
-                //    {
-                //        result = number; 
-                //    }
-                //}
-
-
-                //if (number % 2 == 0)
-                //{
-                //    result += number;
-                //}
-
-
-                //result++;
-                //if (number % 2 == 0)
-                //{
-                //    result = result + 1;
-                //}
-
-                if (number % 2 == 0)
+                if (!result.HasValue || str.Length > result)
                 {
-                    sum += number;
-                    total++;
+                    result = str.Length;
                 }
-
-
             }
 
-            result = sum / total;
-            
-            Console.WriteLine(result);
+            //Console.WriteLine("The shortest country name has {0} characters in its name",
+            //    result);
+            Console.WriteLine("The longest country name has {0} characters in its name",
+                result);
+
         }
     }
 }
--- pragrimtech_linq_vs_2019_/LINQDemo/Program.cs
+++ pragrimtech_linq_vs_2019_/LINQDemo/Program.cs
@@ -7,35 +7,16 @@ namespace LINQDemo
     {
         static void Main(string[] args)
         {
-            int[] Numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            string[] countries = { "india", "USA", "UK" };
 
-            //int result = Numbers.Min();
+            //int result = countries.Min(x => x.Length);
+            int result = countries.Max(x => x.Length);
 
-            //  Where will return a list of integers as all even numbers i.e filer them outs
-            //  from that list, Min will then return the smallest even number
-            //  
-            //int result = Numbers.Where(x => x % 2 == 0).Min();
+            //Console.WriteLine("The shortest country name has {0} characters in its name",
+            //    result);
+            Console.WriteLine("The largest country name has {0} characters in its name",
+                result);
 
-            //int result = Numbers.Where(x => x % 2 == 0).Max();
-
-
-            //int result = Numbers.Sum();
-
-            //int result = Numbers.Where(x => x % 2 == 0).Sum();
-
-
-            //int result = Numbers.Count();
-
-            //int result = Numbers.Where(x => x % 2 == 0).Count();
-
-
-            //double result = Numbers.Average();
-
-            double result = Numbers.Where(x => x % 2 == 0).Average();
-
-
-
-            Console.WriteLine(result);
         }
     }
 }
