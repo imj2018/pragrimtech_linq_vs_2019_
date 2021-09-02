429f11f006ae35f59d171b7bf8c33f79eb6b3726
imj <imj.programming.2013@gmail.com>
2021-09-02 18:55:42 +1000

    Part 3   Extension Methods in C#

--- pragrimtech_linq_vs_2019_/pragrimtech_linq_vs_2019_/Program.cs
+++ pragrimtech_linq_vs_2019_/pragrimtech_linq_vs_2019_/Program.cs
@@ -8,17 +8,27 @@ namespace pragrimtech_linq_vs_2019_
     {
         static void Main(string[] args)
         {
-            int[] numbers = { 1, 2, 4, 5, 6, 7, 8, 9, 10 };
+            string strName = "pragim";
+            string result = strName.ChangeFirstLetterCase();
 
-            IEnumerable<int> evenNumbers = from number in numbers
-                                            where (number % 2) == 0
-                                            select number;
 
-            foreach (int item in evenNumbers)
+            //string result = StringHelper.ChangeFirstLetterCase(strName);
+
+
+            List<int> Numbers = new List<int> { 1, 2, 3, 5, 5, 6, 7, 8, 9, 10 };
+            //IEnumerable<int> EvenNumbers = Numbers.Where(n => n % 2 == 0);
+
+
+            IEnumerable<int> EvenNumbers = Enumerable.Where(Numbers, n => n % 2 == 0);
+
+
+            foreach (int evenNumber in EvenNumbers)
             {
-                Console.WriteLine(item);
+                Console.WriteLine(evenNumber);
             }
-            
+
+            Console.WriteLine(result);
+
         }
     }
 }
---
+++ pragrimtech_linq_vs_2019_/pragrimtech_linq_vs_2019_/StringHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pragrimtech_linq_vs_2019_
+{
+    /// <summary>
+    ///  helper i.e wrapper class
+    /// 
+    /// </summary>
+    public static class StringHelper
+    {
+        public static string ChangeFirstLetterCase(this string inputString)
+        {
+            if (inputString.Length > 0)
+            {
+                char[] charArray = inputString.ToCharArray();
+                charArray[0] = char.IsUpper(charArray[0])
+                    ? char.ToLower(charArray[0])
+                    : char.ToUpper(charArray[0]);
+                return new string(charArray);
+            }
+
+            return inputString;
+        }
+    }
+}
