a6644415c94905217101fd30d3a11e7c28a35956
imj <imj.programming.2013@gmail.com>
2021-09-02 11:23:28 +1000

    first commit

---
+++ pragrimtech_linq_vs_2019_/pragrimtech_linq_vs_2019_/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pragrimtech_linq_vs_2019_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
