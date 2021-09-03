fbc953e96218d3ac93d08ed608ed5c01cde7df30
imj <imj.programming.2013@gmail.com>
2021-09-03 20:36:46 +1000

    Part 6   Restriction Operators in LINQ (did not do ADO.NET example)

--- pragrimtech_linq_vs_2019_/Demo/Program.cs
+++ pragrimtech_linq_vs_2019_/Demo/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Demo
 {
@@ -6,33 +8,85 @@ namespace Demo
     {
         static void Main(string[] args)
         {
-            //string[] countries = { "India", "USA", "UK", "Canada", "Australia" };
+            List<int> numbers = new List<int> { 1, 2, 4, 5, 6, 7, 8, 9, 10 };
 
-            //string result = string.Empty;
+            //  the lambda matches the requirements of the Func<int, bool>
+            //  x is int, x % 2 == 0 is a bool e.g if it were x + 2 for the expression
+            //  it would return an error as it return an int
+            // 
+            //IEnumerable<int> evenNumbers = numbers.Where(x => x % 2 == 0);
 
-            //foreach (var str in countries)
+
+            //  explicitely use and pass a Func (generic delegate)
+            //  
+            //Func<int, bool> predicate = x => x % 2 == 0;
+            //IEnumerable<int> evenNumbers = numbers.Where(predicate);
+
+
+            //IEnumerable<int> evenNumbers = numbers.Where(num => isEven(num));
+
+
+            //  look like SQL, T-SQL
+            //
+            //IEnumerable<int> evenNumbers = from num in numbers
+            //                               where num % 2 == 0
+            //                               select num;
+
+
+            //  the second parameter can be used to get the index of each
+            //
+            //  this will throw an error as it is returning an anonymous type therefore
+            //  the var keyword can be used, it will contain IEnumerable of the anonymous 
+            //  type new { Number = num, Index = index }
+            //
+            //var result = numbers.Select((num, index) => new { Number = num, Index = index });
+
+            //foreach (var item in result)
             //{
-            //    //result = result + str + ", ";
-            //    result += str + ", ";
+            //    Console.WriteLine(item.Number + " - " + item.Index);
             //}
 
-            //int lastIndex = result.LastIndexOf(",");
-            //result = result.Remove(lastIndex);
 
-            //Console.WriteLine(result);
+            //  only even numbers
+            //
+            //var result = numbers
+            //    .Select((num, index) => new { Number = num, Index = index })
+            //    .Where(x => x.Number % 2 == 0);
 
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item.Number + " - " + item.Index);
+            //}
 
-            int[] Numbers = { 2, 3, 4, 5 };
+            //  returning only the Index it will only return the Index which is
+            //  type integer dnot as an anonymous object unlike before
+            //
+            var result = numbers
+                .Select((num, index) => new { Number = num, Index = index })
+                .Where(x => x.Number % 2 == 0)
+                .Select(x => x.Index);
 
-            int result = 1;
-
-            foreach (int number in Numbers)
+            foreach (var item in result)
             {
-                result = result * number;
+                Console.WriteLine(item);
             }
 
-            Console.WriteLine(result);
 
         }
+
+        private static bool isEven(int number)
+        {
+            //if (number % 2 == 0)
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+
+            return number % 2 == 0;
+        }
+
     }
 }
