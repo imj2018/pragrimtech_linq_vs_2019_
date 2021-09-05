28073d27f18e50c774e7be7870945da89957b865
imj <imj.programming.2013@gmail.com>
2021-09-05 21:20:32 +1000

    Part 9   Difference between Select and SelectMany in LINQ

--- pragrimtech_linq_vs_2019_/Demo/Program.cs
+++ pragrimtech_linq_vs_2019_/Demo/Program.cs
@@ -8,87 +8,31 @@ namespace Demo
     {
         static void Main(string[] args)
         {
-            //  SelectMany to retrieve all the students subjects
-            // 
-            //  the lambda expression is applied to all student objects in the List of students
-            // 
-            //  "What is Select many going to do, this is going to project all the subjects of a
-            //  given student to a single IEnumerable of string... We have got 4 students so there
-            //  will be 4 IEnumberable of strings, all of them will be flattened/combined to form a single
-            //  IEnumberable of string"
+            //  Select returns an IEnumerable List of List of strings
+            //  
+            IEnumerable<List<string>> result = Student.GetAllStudents().Select(x => x.Subjects);
+
+            //  as it is returning a List of List of strings a nested foreach is needed
             //
-            //IEnumerable<string> subjects = Student.GetAllStudents().SelectMany(s => s.Subjects);
-
-            //  SQL like syntax
-            //
-            //  the second from clause will get its data from the first from clause i.e 
-            //  the first from clause will get the List of all students, the second will
-            //  will get all the Subjects
-            // 
-            //IEnumerable<string> subjects = from student in Student.GetAllStudents()
-            //                               from subject in student.Subjects
-            //                               select subject;
-
-
-            string[] stringArray =
+            foreach (List<string> stringList in result)
             {
-                "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-                "0123456789"
-            };
+                foreach (string str in stringList)
+                {
+                    Console.WriteLine(str);
+                }
+            }
+            Console.WriteLine("");
 
-            //  two IEnumberable of character sequences that will be flattened/combined to form a single
-            //  IEnumberable of character sequence
-            //
-            //IEnumerable<char> result = stringArray.SelectMany(s => s);
 
-            //  SQL like syntax
-            //
-            //IEnumerable<char> result = from s in stringArray
-            //                           from c in s
-            //                           select c;
-
-            //foreach (char c in result)
-            //{
-            //    Console.WriteLine(c);
-            //}
-            //Console.WriteLine("");
-
-            //  multiple students are enrolled in C#, Distince can be used
-            //  to filter out multiple results that are the same
-            //  
-            //IEnumerable<string> subjects = Student.GetAllStudents().SelectMany(s => s.Subjects).Distinct();
-
-            //  SQL like syntax
-            //
-            //  invoke Distict on the results on this query i.e use of open brackets
+            //  SelectMany is returning just an IEnumberable of string unlike Select it is not
+            //  returning a List of List of a given type, it flattens the List of a given type 
+            //  into a single List
             // 
-            //IEnumerable<string> subjects = (from student in Student.GetAllStudents()
-            //                              from subject in student.Subjects
-            //                              select subject).Distinct();
+            IEnumerable<string> resultSelectMany = Student.GetAllStudents().SelectMany(x => x.Subjects);
 
-
-            //  the student name and subject
-            //  
-            //  s => s.Subjects to get the List of subjects, create two variables (student, subject)
-            //  then project into a new anonymous type 
-            // 
-            //  the StudentName will get all the names front the student variable, the other property
-            //  SubjectName will get it's value from subject
-            //  
-            var result = Student.GetAllStudents()
-                .SelectMany(s => s.Subjects, 
-                (student, subject) => new { 
-                    StudentName = student.Name, 
-                    SubjectName = subject 
-                } );
-
-            var resultSQL = from student in Student.GetAllStudents()
-                            from subject in student.Subjects
-                            select new { StudentName = student.Name, SubjectName = subject };
-
-            foreach (var v in resultSQL)
+            foreach (string str in resultSelectMany)
             {
-                Console.WriteLine(v.StudentName + " - " + v.SubjectName);
+                Console.WriteLine(str);
             }
 
         }
--- pragrimtech_linq_vs_2019_/Demo/Student.cs
+++ pragrimtech_linq_vs_2019_/Demo/Student.cs
@@ -16,8 +16,8 @@ namespace Demo
             {
                 new Student { Name = "Tom", Gender = "Male", Subjects = new List<string> { "ASP.NET", "C#" } },
                 new Student { Name = "Mike", Gender = "Male", Subjects = new List<string> { "ADO.NET", "C#", "AJAX" } },
-                new Student { Name = "Pam", Gender = "Male", Subjects = new List<string> { "WCF.NET", "SQL Server", "C#" } },
-                new Student { Name = "Marry", Gender = "Male", Subjects = new List<string> { "WPF", "LINQ", "ASP.NET" } },
+                new Student { Name = "Pam", Gender = "Female", Subjects = new List<string> { "WCF.NET", "SQL Server", "C#" } },
+                new Student { Name = "Marry", Gender = "Female", Subjects = new List<string> { "WPF", "LINQ", "ASP.NET" } },
             };
 
             return listStudents;
