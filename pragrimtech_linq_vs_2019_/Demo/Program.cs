1768660ecb35a010c28d1fdd6a8f5efcc6c3c3fc
imj <imj.programming.2013@gmail.com>
2021-09-06 12:59:01 +1000

    Part 13   Implement paging using skip and take operators

--- pragrimtech_linq_vs_2019_/Demo/Program.cs
+++ pragrimtech_linq_vs_2019_/Demo/Program.cs
@@ -8,44 +8,55 @@ namespace Demo
     {
         static void Main(string[] args)
         {
-            string[] countries = { "Australia", "Canada", "Germany", "US", "India", "UK", "Italy" };
-
-            //  Take will return the first 3 country names   
-            //
-            IEnumerable<string> result = countries.Take(3);
-
-            //  SQL like
-            //
-            IEnumerable<string> resultSql = (from country in countries
-                                            select country).Take(3);
-
-
-            //  Skip will skip the elements then return the remaining  
-            // 
-            IEnumerable<string> resultSkip = countries.Skip(3);
-
-
-            //  TakeWhile, the lambda will be executed against every item to check if true
-            //  then return each string greater than 2
-            // 
-            IEnumerable<string> resultTakeWhile = countries.TakeWhile(c => c.Length > 2);
-
-            IEnumerable<string> resultTakeWhileSql = (from c in countries
-                                                      select c).TakeWhile(c => c.Length > 2);
-
-
-            //  SkipWhile will execute until the condition is false i.e it gets to US 
-            //  then will return the remaning 
-            //  
-            IEnumerable<string> resultSkipWhile = countries.SkipWhile(c => c.Length > 2);
-
-            IEnumerable<string> resultSkipWhileSql = (from c in countries
-                                                      select c).SkipWhile(c => c.Length > 2);
-
-            foreach (var str in resultSkipWhileSql)
+            do
             {
-                Console.WriteLine(str);
-            }
+                IEnumerable<Student> students = Student.GetAllStudents();
+
+                Console.WriteLine("Please enter Page Number - 1,2,3 or 4");
+
+                int pageNumber = 0;
+
+                if (int.TryParse(Console.ReadLine(), out pageNumber))
+                {
+                    if (pageNumber >= 1 && pageNumber <= 4)
+                    {
+                        int pageSize = 3;
+
+                        //  e.g if the user has entered page 1 get the first 3 records 
+                        //  and do not skip any records. if the user enters 1 then 1 - 1 = 0 * 3
+                        //  is 0 so no pages will be skipped.
+                        //  
+                        //students.Skip((pageNumber - 1) * pageSize);
+
+                        //  Take 3 records or objects i.e pageSize
+                        //  if user enters 2 then 2 - 1 = 1 * 3 is 3, so skip 3 for then take/get
+                        //  the next 3 i.e 104, 105, 106
+                        //  
+                        //students.Skip((pageNumber - 1) * pageSize).Take(pageSize);
+
+                        //  store them to print
+                        IEnumerable<Student> result = students.Skip((pageNumber - 1) * pageSize).Take(pageSize);
+
+                        Console.WriteLine("");
+                        Console.WriteLine("Displaying Page " + pageNumber);
+                        foreach (Student student in result)
+                        {
+                            Console.WriteLine(student.StudentID + "\t" + student.Name + "\t" + student.TotalMarks);
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Page number must be an integer between 1 and 4");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Page number must be an integer between 1 and 4");
+                }
+
+            } while (true);
+
         }
 
     }
--- pragrimtech_linq_vs_2019_/Demo/Student.cs
+++ pragrimtech_linq_vs_2019_/Demo/Student.cs
@@ -18,11 +18,16 @@ namespace Demo
             List<Student> listStudents = new List<Student>()
             {
                 new Student { StudentID = 101, TotalMarks = 800, Name = "Tom", Gender = "Male", Subjects = new List<string> { "ASP.NET", "C#" } },
-                new Student { StudentID = 104, TotalMarks = 800, Name = "John", Gender = "Male", Subjects = new List<string> { "ADO.NET", "C#", "AJAX" } },
-                new Student { StudentID = 105, TotalMarks = 800, Name = "John", Gender = "Male", Subjects = new List<string> { "WCF.NET", "SQL Server", "C#" } },
                 new Student { StudentID = 102, TotalMarks = 900, Name = "Mary", Gender = "Female", Subjects = new List<string> { "WPF", "LINQ", "ASP.NET" } },
                 new Student { StudentID = 103, TotalMarks = 800, Name = "Pam", Gender = "Female", Subjects = new List<string> { "WPF", "LINQ", "ASP.NET" } },
-
+                new Student { StudentID = 104, TotalMarks = 800, Name = "John", Gender = "Male", Subjects = new List<string> { "ADO.NET", "C#", "AJAX" } },
+                new Student { StudentID = 105, TotalMarks = 800, Name = "John", Gender = "Male", Subjects = new List<string> { "WCF.NET", "SQL Server", "C#" } },              
+                new Student { StudentID = 106, TotalMarks = 700, Name = "Brian", Gender = "Male", Subjects = new List<string> { "ADO.NET", "C#", "AJAX" } },
+                new Student { StudentID = 107, TotalMarks = 750, Name = "Jade", Gender = "Female", Subjects = new List<string> { "ASP.NET", "C#" } },
+                new Student { StudentID = 108, TotalMarks = 850, Name = "Ron", Gender = "Male", Subjects = new List<string> { "WPF", "LINQ", "ASP.NET" } },
+                new Student { StudentID = 109, TotalMarks = 750, Name = "Alex", Gender = "Male", Subjects = new List<string> { "ASP.NET", "C#" } },
+                new Student { StudentID = 110, TotalMarks = 860, Name = "Susan", Gender = "Female", Subjects = new List<string> { "WPF", "LINQ", "ASP.NET" } },
+                new Student { StudentID = 111, TotalMarks = 900, Name = "Rob", Gender = "MAle", Subjects = new List<string> { "WPF", "LINQ", "ASP.NET" } },
 
             };
 
