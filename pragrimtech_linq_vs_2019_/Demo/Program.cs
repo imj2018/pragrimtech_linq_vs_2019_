0c3ca34071008bb6f5482964cb69983f74ebd0cc
imj <imj.programming.2013@gmail.com>
2021-09-10 07:47:32 +1000

    Part 15   Conversion Operators in LINQ

--- pragrimtech_linq_vs_2019_/Demo/Program.cs
+++ pragrimtech_linq_vs_2019_/Demo/Program.cs
@@ -8,57 +8,140 @@ namespace Demo
     {
         static void Main(string[] args)
         {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            //  converting with ToList is a greedy operator
+            //  
+            List<int> result = numbers.ToList();
+
+            foreach (int i in result)
+            {
+                Console.Write(i + "\t");
+            }
+            Console.WriteLine("");
+
+
+            List<string> countries = new List<string>() { "US", "India", "UK", "Australia", "Canada" };
+
+            //  converting with ToArray is a greedy operator
+            //
+            string[] resultArray = countries.OrderBy(c => c).ToArray();
+
+            foreach (string str in resultArray)
+            {
+                Console.Write(str + "\t");
+            }
+            Console.WriteLine("");
+
 
             List<Student> listStudents = new List<Student>()
             {
-                new Student { StudentID = 101, Name = "Tom", TotalMarks = 800 },
-                new Student { StudentID = 102, Name = "Mary", TotalMarks = 900  },
-                new Student { StudentID = 103, Name = "Pam", TotalMarks = 800  }
+                new Student { StudentID = 101, TotalMarks = 800, Name = "Tom", Gender = "Male", Subjects = new List<string> { "ASP.NET", "C#" } },
+                new Student { StudentID = 102, TotalMarks = 900, Name = "Mary", Gender = "Female", Subjects = new List<string> { "WPF", "LINQ", "ASP.NET" } },
+                new Student { StudentID = 103, TotalMarks = 800, Name = "Pam", Gender = "Female", Subjects = new List<string> { "WPF", "LINQ", "ASP.NET" } },
+                //new Student { StudentID = 103, TotalMarks = 800, Name = "XYZ", Gender = "Female", Subjects = new List<string> { "WPF", "LINQ", "ASP.NET" } },
+
             };
 
-
-            //  "LINQ operators can be broadly classified into 2 categories based on the behaviour of query execution
-            //  1.Deferred or Lazy Operators - These query operators use deferred execution.Examples - select, where, Take, Skip etc
-            //  2.Immediate or Greedy Operators - These query operators use immediate execution.Examples - count, average, min, max, ToList etc"
-
-
-            //  if the query executed is executed Tim should not be in the result, 
-            //  Tim is in the result so the query is not executed where is defined therefore
-            //  it is known as deferred execution
-            // 
-            //IEnumerable<Student> result = from student in listStudents
-            //                              where student.TotalMarks == 800
-            //                              select student;
-
-
-            //  an immediate operator like ToList will force the query to execute where it is defined
-            //  therefore Tim will not be added to the List
-            // 
-            //IEnumerable<Student> result = (from student in listStudents
-            //                               where student.TotalMarks == 800
-            //                               select student).ToList();
-
-
-            //  Count is also an immediate or greedy operator
+            //  ToDictionary is also greedy
             //
-           int result = (from student in listStudents
-                         where student.TotalMarks == 800
-                         select student).Count();
+            Dictionary<int, string> dictionaryIDNames =
+                listStudents.ToDictionary(x => x.StudentID, x => x.Name);
+
+            foreach (KeyValuePair<int, string> kvp in dictionaryIDNames)
+            {
+                Console.WriteLine(kvp.Key + "\t" + kvp.Value);
+            }
+            Console.WriteLine("");
 
 
-            listStudents.Add(new Student() { StudentID = 104, Name = "Tim", TotalMarks = 800 });
+            //  by just using the keySelector i.e StudentID the value will be a 
+            //  Student object itself (first overloaded version)
+            // 
+            //Dictionary<int, Student> dicionaryStudents = listStudents.ToDictionary(x => x.StudentID);
+
+            //  can also use elementSelector to specify the value for the same result
+            // 
+            Dictionary<int, Student> dicionaryStudents = listStudents.ToDictionary(x => x.StudentID, x => x);
 
 
-            //foreach (Student student in result)
-            //{
-            //    Console.WriteLine(student.StudentID + "\t" + student.Name + "\t" + student.TotalMarks);           
-            //}
+            foreach (KeyValuePair<int, Student> kvp in dicionaryStudents)
+            {
+                Console.WriteLine(kvp.Key + "\t" + kvp.Value.Name + "\t" + kvp.Value.TotalMarks);
+            }
+            Console.WriteLine("");
 
-            Console.WriteLine("Total = " + result);
+
+            List<Employee> listEmployees = new List<Employee>
+            {
+                new Employee() { Name = "Ben", JobTitle = "Developer", City = "London" },
+                new Employee() { Name = "John", JobTitle = "Sr. Developer", City = "Bangalore" },
+                new Employee() { Name = "Steve", JobTitle = "Developer", City = "Bangalore" },
+                new Employee() { Name = "Stuart", JobTitle = "Sr. Developer", City = "London" },
+                new Employee() { Name = "Sara", JobTitle = "Developer", City = "London" },
+                new Employee() { Name = "Pam", JobTitle = "Developer", City = "London" }
+            };
+            
+
+            //  ILookup will group the employees based on job title
+            //  
+            ILookup<string, Employee> employeesByJobTitle = listEmployees.ToLookup(x => x.JobTitle);
+
+            Console.WriteLine("Employees Grouped by Job Title");
+            
+            foreach (IGrouping<string, Employee> kvp in employeesByJobTitle)
+            {
+                //  first loop will print out the key which is job title
+                // 
+                Console.WriteLine(kvp.Key);
+
+                //  foreach key there are multiple values, employeesByJobTitle is the lookup when opening
+                //  square brackets it immediately asks for a key, as we are iterating through another loop
+                //  we want the employee related to that key
+                foreach (var employee in employeesByJobTitle[kvp.Key])
+                {
+                    Console.WriteLine("\t" + employee.Name + "\t" + employee.JobTitle + "\t" + employee.City);
+                }
+            }
+            Console.WriteLine("");
+
+
+            //  ILookup based on city
+            // 
+            //  the second argument can be an elementSelector, either pass the entire Employee object
+            //  or specific properties (by default it's the entire Employee object)
+            //
+            var employeesByCity = listEmployees.ToLookup(x => x.City);
+
+            Console.WriteLine("Employees Grouped by City");
+
+            foreach (IGrouping<string, Employee> kvp in employeesByCity)
+            {
+                //  first loop will print out the key which is job title
+                // 
+                Console.WriteLine(kvp.Key);
+
+                //  foreach key there are multiple values, employeesByJobTitle is the lookup when opening
+                //  square brackets it immediately asks for a key, as we are iterating through another loop
+                //  we want the employee related to that key
+                foreach (var employee in employeesByCity[kvp.Key])
+                {
+                    Console.WriteLine("\t" + employee.Name + "\t" + employee.JobTitle + "\t" + employee.City);
+                }
+            }
 
 
 
         }
+
+        public class Employee
+        {
+            public string Name { get; set; }
+            public string JobTitle { get; set; }
+            public string City { get; set; }
+        }
+
+
 
     }
 }
