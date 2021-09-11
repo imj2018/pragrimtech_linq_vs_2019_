c0d34d8bfd80d3403ab304c67c7d1fab2b9e44df
imj <imj.programming.2013@gmail.com>
2021-09-11 22:33:49 +1000

    Part 18   GroupBy in LINQ

--- pragrimtech_linq_vs_2019_/Demo/Employee.cs
+++ pragrimtech_linq_vs_2019_/Demo/Employee.cs
@@ -6,51 +6,40 @@ namespace Demo
 {
     public class Employee
     {
-        public int EmployeeID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public int ID { get; set; }
+        public string Name { get; set; }
         public string Gender { get; set; }
-        public int AnnualSalary { get; set; }
+        public string Department { get; set; }
+        public int Salary { get; set; }
+
 
         public static List<Employee> GetAllEmployees()
         {
-            List<Employee> listEmployees = new List<Employee>
-            {
-            new Employee
-            {
-                EmployeeID = 101,
-                FirstName = "Tom",
-                LastName = "Daely",
-                Gender = "Male",
-                AnnualSalary = 60000
-            },
-            new Employee
-            {
-                EmployeeID = 102,
-                FirstName = "Mike",
-                LastName = "Mist",
-                Gender = "Male",
-                AnnualSalary = 72000
-            },
-            new Employee
-            {
-                EmployeeID = 103,
-                FirstName = "Mary",
-                LastName = "Lambeth",
-                Gender = "Female",
-                AnnualSalary = 48000
-            },
-            new Employee
-            {
-                EmployeeID = 104,
-                FirstName = "Pam",
-                LastName = "Penny",
-                Gender = "Female",
-                AnnualSalary = 84000
-            },
-            };
+            return new List<Employee>()
+        {
+            new Employee { ID = 1, Name = "Mark", Gender = "Male",
+                                         Department = "IT", Salary = 45000 },
+            new Employee { ID = 2, Name = "Steve", Gender = "Male",
+                                         Department = "HR", Salary = 55000 },
+            new Employee { ID = 3, Name = "Ben", Gender = "Male",
+                                         Department = "IT", Salary = 65000 },
+            new Employee { ID = 4, Name = "Philip", Gender = "Male",
+                                         Department = "IT", Salary = 55000 },
+            new Employee { ID = 5, Name = "Mary", Gender = "Female",
+                                         Department = "HR", Salary = 48000 },
+            new Employee { ID = 6, Name = "Valarie", Gender = "Female",
+                                         Department = "HR", Salary = 70000 },
+            new Employee { ID = 7, Name = "John", Gender = "Male",
+                                         Department = "IT", Salary = 64000 },
+            new Employee { ID = 8, Name = "Pam", Gender = "Female",
+                                         Department = "IT", Salary = 54000 },
+            new Employee { ID = 9, Name = "Stacey", Gender = "Female",
+                                         Department = "HR", Salary = 84000 },
+            new Employee { ID = 10, Name = "Andy", Gender = "Male",
+                                         Department = "IT", Salary = 36000 }
+        };
 
-            return listEmployees;
         }
+
     }
 }
--- pragrimtech_linq_vs_2019_/Demo/Program.cs
+++ pragrimtech_linq_vs_2019_/Demo/Program.cs
@@ -6,41 +6,88 @@ using System.Linq;
 namespace Demo
 {
     /// <summary>
-    ///  attempt without sql db
     ///  
+    /// 
     /// </summary>
     class Program
     {
 
         public static void Main()
         {
-            List<Employee> employees = Employee.GetAllEmployees();
-
-            
-            IEnumerable<Employee> result = employees
-
-                //  "So in short, use AsEnumberable operator to move query processing to the client side"
-
-                //  AsEnumerable will break the query in 2 parts
-                //  before AsEnumerable i.e the INSIDE PART, is called as a select all from in SQL which is
-                //  executed as Linq-to-SQL
-                // 
-                //.AsEnumerable()
-
-                //  the second part i.e OUTSIDE PART is executed after AsEnumerable on the data/objects in memory
-                //  Where, OrderByDescending and Take are applied to them as Linq-to-Objects on the client side
-                // 
-                .Where(x => x.Gender == "Male")
-
-                //  e.g if AsEnumerable is put here it will also include the WHERE clause in the SQL
-                .AsEnumerable()
-                .OrderByDescending(x => x.AnnualSalary)
-                .Take(5);
+            //  GroupBy will return IEnumberable<IGrouping<string, Employee>>
+            //
+            //  employeeGroups will store two groups of employees, one group will have the key
+            //  "HR" with all employees belonging to the "HR" Department the other will have the "IT"...
+            //
+            //var employeeGroups = Employee.GetAllEmployees().GroupBy(x => x.Department);
 
 
-            foreach (var e in result)
+            //  SQL like
+            //var employeeGroups = from employee in Employee.GetAllEmployees()
+            //                     group employee by employee.Department;
+
+
+            //  there is no sorting for the employees or the departments
+            //
+            //  we want to sort the departments first in ascending order, then each employee within
+            //  each department should be sorted
+            //
+            var employeeGroups = from employee in Employee.GetAllEmployees()
+
+                                     //  store the employees in a variable called eGroup
+                                     //
+                                 group employee by employee.Department into eGroup
+
+                                 //  orderby by the Key property which is the department name
+                                 //  so the groups will be sorted by Key in ascending order i.e HR then IT 
+                                 //
+                                 orderby eGroup.Key
+
+                                 //  project a new anonymous type, employeeGroups will now have
+                                 //  two groups with these two properties
+                                 // 
+                                 select new
+                                 {
+                                     Key = eGroup.Key,
+                                     //Employees = eGroup
+
+                                     //  call OrderBy to sort the names as they are not in ascending order
+                                     Employees = eGroup.OrderBy(x => x.Name)
+                                 };
+
+
+
+            foreach (var group in employeeGroups)
             {
-                Console.WriteLine(e.FirstName + "\t" + e.AnnualSalary + "\t" + e.Gender); 
+                //  to reiterate these are all aggregate functions
+                // 
+                //Console.WriteLine("{0} - {1}", group.Key, group.Count());
+                //Console.WriteLine("{0} - {1}", group.Key, group.Count(x => x.Gender == "Male"));
+                //Console.WriteLine("{0} - {1}", group.Key, group.Max(x => x.Salary));
+                //Console.WriteLine("{0} - {1}", group.Key, group.Min(x => x.Salary));
+                //Console.WriteLine("{0} - {1}", group.Key, group.Sum(x => x.Salary));
+
+
+                //Console.WriteLine("{0} - {1}", group.Key, group.Count());
+                Console.WriteLine("{0} - {1}", group.Key, group.Employees.Count());
+                Console.WriteLine("------------");
+
+                //  group contains the Department as a the key and the employees belonging to that group
+                //  therefore we can loop through to get each employee object in the group
+                //
+                //foreach (var employee in group)
+
+                //  the employees are no longer present in group, they are now in the Employees
+                //  property i.e they have been projected into the Employees property
+                //
+                foreach (var employee in group.Employees)
+                {
+                    Console.WriteLine(employee.Name + "\t" + employee.Department);
+                }
+                Console.WriteLine("");
+                Console.WriteLine("");
+
+
             }
 
 
