549c9de30a1680632b8dc8c15910e52d64e55a98
imj <imj.programming.2013@gmail.com>
2021-09-11 22:45:42 +1000

    broke something

--- pragrimtech_linq_vs_2019_/Demo/Program.cs
+++ pragrimtech_linq_vs_2019_/Demo/Program.cs
@@ -6,7 +6,7 @@ using System.Linq;
 namespace Demo
 {
     /// <summary>
-    /// test ammend
+    /// 
     /// 
     /// </summary>
     class Program
@@ -14,30 +14,77 @@ namespace Demo
 
         public static void Main()
         {
-   
-            var employeeGroups = from employee in Employee.GetAllEmployees()              
+            //  GroupBy will return IEnumberable<IGrouping<string, Employee>>
+            //
+            //  employeeGroups will store two groups of employees, one group will have the key
+            //  "HR" with all employees belonging to the "HR" Department the other will have the "IT"...
+            //
+            //var employeeGroups = Employee.GetAllEmployees().GroupBy(x => x.Department);
+
+
+            //  SQL like
+            //var employeeGroups = from employee in Employee.GetAllEmployees()
+            //                     group employee by employee.Department;
+
+
+            //  there is no sorting for the employees or the departments
+            //
+            //  we want to sort the departments first in ascending order, then each employee within
+            //  each department should be sorted
+            //
+            var employeeGroups = from employee in Employee.GetAllEmployees()
+
+                                     //  store the employees in a variable called eGroup
+                                     //
                                  group employee by employee.Department into eGroup
+
+                                 //  orderby by the Key property which is the department name
+                                 //  so the groups will be sorted by Key in ascending order i.e HR then IT 
+                                 //
                                  orderby eGroup.Key
+
+                                 //  project a new anonymous type, employeeGroups will now have
+                                 //  two groups with these two properties
+                                 // 
                                  select new
                                  {
                                      Key = eGroup.Key,
+                                     //Employees = eGroup
+
+                                     //  call OrderBy to sort the names as they are not in ascending order
                                      Employees = eGroup.OrderBy(x => x.Name)
                                  };
 
 
-
             foreach (var group in employeeGroups)
             {
+                //  to reiterate these are all aggregate functions
+                // 
+                //Console.WriteLine("{0} - {1}", group.Key, group.Count());
+                //Console.WriteLine("{0} - {1}", group.Key, group.Count(x => x.Gender == "Male"));
+                //Console.WriteLine("{0} - {1}", group.Key, group.Max(x => x.Salary));
+                //Console.WriteLine("{0} - {1}", group.Key, group.Min(x => x.Salary));
+                //Console.WriteLine("{0} - {1}", group.Key, group.Sum(x => x.Salary));
+
+
+                //Console.WriteLine("{0} - {1}", group.Key, group.Count());
                 Console.WriteLine("{0} - {1}", group.Key, group.Employees.Count());
                 Console.WriteLine("------------");
 
+                //  group contains the Department as a the key and the employees belonging to that group
+                //  therefore we can loop through to get each employee object in the group
+                //
+                //foreach (var employee in group)
+
+                //  the employees are no longer present in group, they are now in the Employees
+                //  property i.e they have been projected into the Employees property
+                //
                 foreach (var employee in group.Employees)
                 {
                     Console.WriteLine(employee.Name + "\t" + employee.Department);
                 }
                 Console.WriteLine("");
                 Console.WriteLine("");
-
 
             }
 
