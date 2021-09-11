444378757452d5893ca37e601d9f015dcd513525
imj <imj.programming.2013@gmail.com>
2021-09-11 19:36:20 +1000

    Part 17   AsEnumerable and AsQueryable in LINQ (attempt without sql db)

--- pragrimtech_linq_vs_2019_/Demo/Program.cs
+++ pragrimtech_linq_vs_2019_/Demo/Program.cs
@@ -5,47 +5,50 @@ using System.Linq;
 
 namespace Demo
 {
+    /// <summary>
+    ///  attempt without sql db
+    ///  
+    /// </summary>
     class Program
     {
 
         public static void Main()
         {
-            ArrayList list = new ArrayList()
+            List<Employee> employees = Employee.GetAllEmployees();
+
+            
+            IEnumerable<Employee> result = employees
+
+                //  "So in short, use AsEnumberable operator to move query processing to the client side"
+
+                //  AsEnumerable will break the query in 2 parts
+                //  before AsEnumerable i.e the INSIDE PART, is called as a select all from in SQL which is
+                //  executed as Linq-to-SQL
+                // 
+                //.AsEnumerable()
+
+                //  the second part i.e OUTSIDE PART is executed after AsEnumerable on the data/objects in memory
+                //  Where, OrderByDescending and Take are applied to them as Linq-to-Objects on the client side
+                // 
+                .Where(x => x.Gender == "Male")
+
+                //  e.g if AsEnumerable is put here it will also include the WHERE clause in the SQL
+                .AsEnumerable()
+                .OrderByDescending(x => x.AnnualSalary)
+                .Take(5);
+
+
+            foreach (var e in result)
             {
-                1,
-                2,
-                3,
-
-                //  will throw an error as Cast attempts to cast all items to type int
-                //
-                "4",
-                "ABC"
-            };
-
-
-            //  Cast will attempt to cast all elements to type int
-            //  as it is deferred/lazy it will be thrown in the loop
-            // 
-            IEnumerable<int> result = list.Cast<int>();
-
-
-            //  will only return elements of that type 
-            //
-            IEnumerable<int> resultOfType = list.OfType<int>();
-
-
-            foreach (int i in resultOfType)           
-            {
-                Console.WriteLine(i);
+                Console.WriteLine(e.FirstName + "\t" + e.AnnualSalary + "\t" + e.Gender); 
             }
-
-
-
 
 
         }
 
 
     }
+
+
 
 }
