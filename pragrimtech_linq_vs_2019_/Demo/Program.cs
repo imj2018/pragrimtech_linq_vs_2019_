68f96fd3f4997a723ae870011dceab29ced9aa78
imj <imj.programming.2013@gmail.com>
2021-09-14 19:41:28 +1000

    revision/relearn 18 to 25 when needed? likely

--- pragrimtech_linq_vs_2019_/Demo/Program.cs
+++ pragrimtech_linq_vs_2019_/Demo/Program.cs
@@ -16,40 +16,7 @@ namespace Demo
 
         public static void Main()
         {
-            //  Cross Join/Cartesian product will be the product of the two sequences which is 10, does not use
-            //  the on keyword 
-            //
-            //var result = from e in Employee.GetAllEmployees()
-            //             from d in Department.GetAllDepartments()
-
-
-            var result = from d in Department.GetAllDepartments()
-                         from e in Employee.GetAllEmployees()
-                         select new { e, d };
-
-
-            foreach (var v in result)
-            {
-                Console.WriteLine(v.e.Name + "\t" + v.d.Name);
-            }
-            Console.WriteLine("");
-
-
-            //var resultMethod = Employee.GetAllEmployees()
-            //                           .SelectMany(e => Department.GetAllDepartments(),
-            //                           (e, d) => new { e, d });
-
-            var resultMethod = Employee.GetAllEmployees().Join(Department.GetAllDepartments(),
-                                                            e => true,
-                                                            d => true,
-                                                            (e, d) => new { e, d });
-
-            foreach (var v in resultMethod)
-            {
-                Console.WriteLine(v.e.Name + "\t" + v.d.Name);
-            }
-            Console.WriteLine("");
-
+            
 
         }
 
