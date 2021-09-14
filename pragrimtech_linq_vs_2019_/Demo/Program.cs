e13636de95ddeaa26b7884055a1d844a0a0fba30
imj <imj.programming.2013@gmail.com>
2021-09-14 20:41:11 +1000

    Part 27   Union, Intersect and Except operators in LINQ

--- pragrimtech_linq_vs_2019_/Demo/Program.cs
+++ pragrimtech_linq_vs_2019_/Demo/Program.cs
@@ -16,10 +16,14 @@ namespace Demo
 
         public static void Main()
         {
-            string[] countries = { "USA", "usa", "INDIA", "UK", "UK" };
-            
-            //var result = countries.Distinct();
-            var result = countries.Distinct(StringComparer.OrdinalIgnoreCase);
+
+
+            int[] numbers1 = { 1, 2, 3, 4, 5 };
+            int[] numbers2 = { 1, 3, 6, 7, 8 };
+
+            //  "Union combines two collections into one collection while removing duplicate elements."
+            //
+            var result = numbers1.Union(numbers2);
 
             foreach (var v in result)
             {
@@ -28,31 +32,47 @@ namespace Demo
             Console.WriteLine("");
 
 
-            List<Employee> employees = new List<Employee>()
+            List<Employee> employees1 = new List<Employee>()
             {
                 new Employee { ID = 101, Name = "Mike"},
-                new Employee { ID = 101, Name = "Mike"},
-                new Employee { ID = 102, Name = "Mary" },
+                new Employee { ID = 102, Name = "Susy"},
+                new Employee { ID = 103, Name = "Mary" },
             };
 
+            List<Employee> employees2 = new List<Employee>()
+            {
+                new Employee { ID = 101, Name = "Mike"},
+                new Employee { ID = 104, Name = "John"}
+            };
 
-            //var resultComplex = employees.Distinct();
-            //var resultComplex = employees.Distinct(new EmployeeComparer());
-
-            //  now knows how to compare objects by overriding Equals method
+            //  like Distinct will not compare the properties of the object, can be solved
+            //  the same way by projecting an anonymous type
             // 
-            //var resultComplex = employees.Distinct();
-
-            //  can just into a new anonymous type and the and none of the above are needed
-            //  anonymous types automatically override
-            //  
-            var resultComplex = employees.Select(x => new { x.ID, x.Name}).Distinct();
+            //var resultComplex = employees1.Union(employees2);
+            var resultComplex = employees1.Select(x => new { x.ID, x.Name })
+                                          .Union(employees2.Select(x => new { x.ID, x.Name }));
 
             foreach (var v in resultComplex)
             {
                 Console.WriteLine(v.ID + "\t" + v.Name);
             }
+            Console.WriteLine("");
 
+
+            var resultIntersect = numbers1.Intersect(numbers2);
+
+            foreach (var v in resultIntersect)
+            {
+                Console.WriteLine(v);
+            }
+            Console.WriteLine("");
+
+            var resultExcept = numbers1.Except(numbers2);
+
+            foreach (var v in resultExcept)
+            {
+                Console.WriteLine(v);
+            }
 
 
         }
