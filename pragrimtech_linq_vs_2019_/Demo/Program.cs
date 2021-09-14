f29b93f8fe6278355fb720762902fce0e6a8c0d8
imj <imj.programming.2013@gmail.com>
2021-09-14 21:36:39 +1000

    Part 30   SequenceEqual Operator in LINQ

--- pragrimtech_linq_vs_2019_/Demo/Program.cs
+++ pragrimtech_linq_vs_2019_/Demo/Program.cs
@@ -16,18 +16,30 @@ namespace Demo
 
         public static void Main()
         {
-            int[] numbers1 = { 1, 2, 3 };
-            int[] numbers2 = { 1, 4, 5 };
+            string[] countries1 = { "USA", "India", "UK" };
+            //string[] countries2 = { "USA", "India", "UK" };
+            string[] countries2 = { "usa", "India", "uk" };
 
-            //  unlike union Concat will combine both sequences into one sequence but will not
-            //  remove duplicates
-            //
-            var result = numbers1.Concat(numbers2);
+            var result = countries1.SequenceEqual(countries2);  
+            var resultCaseSensitive = countries1.SequenceEqual(countries2, StringComparer.OrdinalIgnoreCase);
 
-            foreach (var v in result) 
-            {
-                Console.WriteLine(v);
-            }
+            Console.WriteLine("Are Equal = " + result);
+            Console.WriteLine("Are Equal = " + resultCaseSensitive);
+            Console.WriteLine("");
+
+            string[] countries3 = { "USA", "INDIA", "UK" };
+            string[] countries4 = { "UK", "INDIA", "USA" };
+
+            var resultUnordered = countries3.SequenceEqual(countries4);
+            var resultOrderBy = countries3.OrderBy(c => c).SequenceEqual(countries4.OrderBy(c => c));
+
+            Console.WriteLine("Are Equal = " + resultUnordered);
+            Console.WriteLine("Are Equal = " + resultOrderBy);
+            Console.WriteLine("");
+
+
+            //  "We discussed implementing these 4 options for Distince in Part26 of LINQ Tutorial. In the same way these options
+            //  can be implemented for SequenceEqual() method"
 
         }
 
